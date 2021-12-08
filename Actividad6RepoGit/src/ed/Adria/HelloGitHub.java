d6ee37211f1b04ce0b5ca9b1c2a32067998a30fc
Caradrian14 <75570220+Caradrian14@users.noreply.github.com>
2021-12-08 15:51:41 +0100

    [MOD] HelloGitHub.java - main() Afegit missatge de benvinguda

--- Actividad6RepoGit/src/ed/Adria/HelloGitHub.java
+++ Actividad6RepoGit/src/ed/Adria/HelloGitHub.java
@@ -3,7 +3,7 @@ package ed.Adria;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola GitHub, soc Adria Jorda");
 
 	}
 
