a1b4f7de5211ffb7719e25f4d262b7a965c62cbd
Caradrian14 <75570220+Caradrian14@users.noreply.github.com>
2021-12-09 12:12:37 +0100

    [mod] metodo nuevo en eclipse

--- Actividad6RepoGit/src/ed/Adria/HelloGitHub.java
+++ Actividad6RepoGit/src/ed/Adria/HelloGitHub.java
@@ -4,7 +4,12 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hola GitHub, soc Adria Jorda");
-
+		mostrar20Numeros();
+	}
+	public static void mostrar20Numeros() {
+		for(int i = 0; i<=20; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
